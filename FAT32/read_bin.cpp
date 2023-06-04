2cfd6275e6c6fea9ed7fcaac778aaeeb8e486d6c
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-06-05 00:06:26 +0900

    [UPDATE] to return int

--- FAT32/read_bin.cpp
+++ FAT32/read_bin.cpp
@@ -1,45 +1,50 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
 #include <string>
 
 using namespace std;
 
-void readBinaryFileBySize(const string& filename, int byteSize) {
+auto readBinaryFileBySize(const string& filename, int byteSize) -> int
+{
     ifstream file(filename, ios::binary);
 
     if (!file) {
         cerr << "no file" << endl;
-        return;
+        return -1;
     }
 
     char* buffer = new char[byteSize];
     file.read(buffer, byteSize);
-    unsigned char value = buffer[0];
-    cout << hex << setw(2) << setfill('0') << static_cast<unsigned int>(value) << " ";
+    
+    int value = 0;
+    for (int i = 0; i < byteSize; i++) {
+        value = (value << 8) | static_cast<unsigned char>(buffer[i]);
+    }
 
     delete[] buffer;
     file.close();
+
+    return value;
 }
 
 int main() {
     string filename = "leaf.jpg";
 
-    // 2바이트씩 읽기
-    cout << "2bytes: " << endl;
-    readBinaryFileBySize(filename, 2);
+    // 2 bytes
+    int value2 = readBinaryFileBySize(filename, 2);
+    cout << "2 bytes: " << hex << value2 << endl;
 
-    // 4바이트씩 읽기
-    cout << "4bytes: " << endl;
-    readBinaryFileBySize(filename, 4);
+    // 4 bytes
+    int value4 = readBinaryFileBySize(filename, 4);
+    cout << "4 bytes: " << hex << value4 << endl;
 
-    // 8바이트씩 읽기
-    cout << "8bytes: " << endl;
-    readBinaryFileBySize(filename, 8);
+    // 8 bytes
+    int value8 = readBinaryFileBySize(filename, 8);
+    cout << "8 bytes: " << hex << value8 << endl;
 
-    // 16바이트씩 읽기
-    cout << "16bytes: " << endl;
-    readBinaryFileBySize(filename, 16);
+    // 16 bytes
+    int value16 = readBinaryFileBySize(filename, 16);
+    cout << "16 bytes: " << hex << value16 << endl;
 
     return 0;
 }

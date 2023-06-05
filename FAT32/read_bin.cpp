24cde5c73bd72ae7718a3f5b155bdd77ca9140cc
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-06-05 17:02:55 +0900

    [UPDATE] to read 1, 2, 4, 8 byte

--- FAT32/read_bin.cpp
+++ FAT32/read_bin.cpp
@@ -1,9 +1,8 @@
-"""
+/*
 [TODO]
-1) little endian으로 읽기
-2) class의 멤버변수로 ifstream 받기
-3) output으로 적어서 저장하기
-"""
+1) class의 멤버변수로 ifstream 받기
+2) output으로 적어서 저장하기
+*/
 
 #include <iostream>
 #include <fstream>
@@ -11,25 +10,37 @@
 
 using namespace std;
 
-auto readBinaryFileBySize(const string& filename, int byteSize) -> int
+auto readBinaryFileBySize(const string& filename, const int& byteSize) -> int
 {
     ifstream file(filename, ios::binary);
 
-    if (!file)
+    if (!file.is_open())
     {
         cerr << "no file" << endl;
         return -1;
     }
 
-    char* buffer = new char[byteSize];
+    unsigned char * buffer = (unsigned char*)malloc(byteSize);
 
+    // 포인터 처음으로 이동
     file.seekg(0, ios::beg);
-    file.read(buffer, byteSize);
+    // bytesize만큼 읽기
+    file.read((char*)buffer, byteSize*2);
     
+    cout << hex << buffer[0] << endl;
+
     int value = 0;
-    for (int i = 0; i < byteSize; i++)
-        // 1바이트 left shiift해서 int에 저장
-        value = (value << 8) | static_cast<unsigned char>(buffer[i]);
+    for (int i = 0; i < byteSize; ++i){
+        /* 
+        little endian으로 읽기 
+            1) 각각 unsigned char로 변환하고
+            2) 8칸 left shift : 앞으로 옮기고 뒤에는 0됨
+            3) or 연산을 통해 다음 값이 뒤에 들어가게 됨
+        즉, 첫 번째 바이트가 가장 낮은 자리, 마지막 바이트가 가장 높은 자리
+        */
+        value = ((value << 8) | buffer[i]);
+        cout << i << "th value is " << hex << value << endl;
+    }
 
     delete[] buffer;
     file.close();
@@ -40,9 +51,13 @@ auto readBinaryFileBySize(const string& filename, int byteSize) -> int
 int main() {
     string filename = "leaf.jpg";
 
+    // 1 bytes
+    int value1 = readBinaryFileBySize(filename, 1);
+    cout << "1 bytes] hex : " <<  value1 << " dec : " << dec << value1 << endl;
     // 2 bytes
     int value2 = readBinaryFileBySize(filename, 2);
     cout << "2 bytes] hex : " << hex << value2 << " dec : " << dec << value2 << endl;
+
     // 4 bytes
     int value4 = readBinaryFileBySize(filename, 4);
     cout << "4 bytes] hex : " << hex << value4 << " dec : " << dec << value4 << endl;
@@ -51,9 +66,35 @@ int main() {
     int value8 = readBinaryFileBySize(filename, 8);
     cout << "8 bytes] hex : " << hex << value8 << " dec : " << dec << value8 << endl;
 
-    // 16 bytes
-    int value16 = readBinaryFileBySize(filename, 16);
-    cout << "16 bytes] hex : " << hex << value16 << " dec : " << dec << value16 << endl;
-
     return 0;
 }
+
+/* 
+FF D8 FF E1 01 B6 45 78 69 66 00 00 4D 4D 00 2A 이 있을때
+
+1) FF를 읽으려면
+FF
+
+2) FF D8 읽으려면
+D8 먼저읽고, 그다음 FF읽기
+D8FF를 변환하기
+
+3) FF D8 FF 읽으려면
+FF 먼저읽고, 그다음 D8읽고, FF읽기
+FFD8FF를 변환하기
+
+4) FF D8 FF E1 읽려면
+E1 먼저읽고, 그다음 FF읽고, D8읽고, FF읽기
+
+...
+    0  1  2  .. n-1
+N) FF D8 .. 읽으려면
+buffer[0] = [n-1]byte
+buffer[1] = [n-2]byte
+...
+buffer[n-1] = [0]byte
+
+==
+
+for(int i=0; i<buffer_size)
+*/
